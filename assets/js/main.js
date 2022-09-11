2772c39f27d55cdd7be37d23d0b0a16a8b138d1c
Mr__Savaliya__ <mrsavaliya8716@gmail.com>
2022-09-11 23:33:46 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,26 @@ $('.counter').each(function() {
   });
 
 // Counter JS Files End 
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
